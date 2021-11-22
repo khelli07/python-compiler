ceac52e5fcfb82e439b702e0adb87224582a12af
alifiarahmah <alifiarahmah03@gmail.com>
2021-11-22 09:46:36 +0700

    refactor

--- testcase/3.5_P02_01.py
+++
@@ -1,9 +0,0 @@
-n = int(input("Masukkan bilangan N: "))
-
-i = n
-jmlbil = 0
-while(i > 0):
-	jmlbil += i % 10
-	i = i // 10
-
-print(jmlbil)
--- testcase/H01_01.py
+++
@@ -1,8 +0,0 @@
-# Program HelloWorld
-# Menuliskan "Hello, World!" ke layar
-
-# KAMUS
-
-# ALGORITMA
-
-print("Hello, World!")
